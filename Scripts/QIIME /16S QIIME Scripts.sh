70f6bb488a3d1aac1b2954cd770f3693b22fb5b8
Gabriel Serrano <gas51591@uga.edu>
2025-07-14 13:38:00 -0400

    Update 16S QIIME Scripts.sh

--- Scripts/QIIME /16S QIIME Scripts.sh
+++ Scripts/QIIME /16S QIIME Scripts.sh
@@ -2,21 +2,21 @@
 # Activate QIIME2/2024.2 and add Path Variables 
 module load QIIME2/2024.2
 INPUT=/home/gas51591/boba/16s/16s_manifest.txt
-OUTPUT=/home/gas51591/boba/16s/analysis/data-sequences.qza
+OUTPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca.qza
 
 # Script
 qiime tools import \
---type 'SampleData[PairedEndSequencesWithQuality]' \
---input-path $INPUT \
---output-path $OUTPUT \
---input-format PairedEndFastqManifestPhred33V2
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path $INPUT \
+  --output-path $OUTPUT \
+  --input-format PairedEndFastqManifestPhred33V2
 
 
 
 # Step 2 Use cutadapt to remove primers
 module load QIIME2/2024.2
-INPUT=/home/gas51591/boba/16s/analysis/data-sequences.qza
-OUTPUT=/home/gas51591/boba/16s/ca_analysis/data-sequences-ca-trimmed.qza
+INPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca.qza
+OUTPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca-trimmed.qza
 
 # Sript
 qiime cutadapt trim-paired \
@@ -31,33 +31,32 @@ qiime cutadapt trim-paired \
 # Step 3 Summarize Demultiplexed Sequences
 # Activate QIIME2/2024.2 and add Path Variables 
 module load QIIME2/2024.2
-INPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca-trimmed.qza
-OUTPUT=/home/gas51591/boba/16s/analysis/data-sequences.qzv
+INPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca.qza
+OUTPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca.qzv
 
 # Script
 qiime demux summarize \
---i-data $INPUT \
---o-visualization $OUTPUT
+  --i-data $INPUT \
+  --o-visualization $OUTPUT
 
 
 
 # Step 4 Denoise with DADA2
-# Activate QIIME2/2024.2-amplicon and add Path Variables 
 module load QIIME2/2024.2-amplicon
 INPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca-trimmed.qza
-OUTPUT=/home/gas51591/boba/16s/analysis/16s-denoised-rep-sequences.qza
+OUTPUT=/home/gas51591/boba/16s/analysis/data-sequences-ca-trimmed-dada2.qza
 
 # Script
 qiime dada2 denoise-paired \
---i-demultiplexed-seqs $INPUT \
---p-trim-left-f 0 \
---p-trunc-len-f 250 \
---p-trim-left-r 0 \
---p-trunc-len-r 200 \
---p-n-threads 12 \
---o-representative-sequences $OUTPUT \
---o-table /home/gas51591/boba/16s/analysis/16s-dada2-table.qza \
---o-denoising-stats /home/gas51591/boba/16s/analysis/16s-dada2-stats.qza
+  --i-demultiplexed-seqs $INPUT \
+  --p-trim-left-f 0 \
+  --p-trunc-len-f 250 \
+  --p-trim-left-r 0 \
+  --p-trunc-len-r 200 \
+  --p-n-threads 12 \
+  --o-representative-sequences $OUTPUT \
+  --o-table /home/gas51591/boba/16s/ca_analysis/16s-dada2-table-ca.qza \
+  --o-denoising-stats /home/gas51591/boba/16s/ca_analysis/16s-dada2-stats-ca.qza
 
 
 
@@ -69,17 +68,17 @@ OUTPUT=/home/gas51591/boba/16s/analysis
 
 # Script
 qiime metadata tabulate \
---m-input-file /home/gas51591/boba/16s/analysis/16s-dada2-stats.qza \
---o-visualization /home/gas51591/boba/16s/analysis/16s-dada2-stats.qzv
+  --m-input-file /home/gas51591/boba/16s/ca_analysis/16s-dada2-stats-ca.qza \
+  --o-visualization /home/gas51591/boba/16s/ca_analysis/16s-dada2-stats-ca.qzv
 
 qiime feature-table summarize \
---i-table /home/gas51591/boba/16s/analysis/16s-dada2-table.qza \
---o-visualization /home/gas51591/boba/16s/analysis/16s-dada2-table.qzv \
---m-sample-metadata-file /home/gas51591/boba/16s/metadata/16s_metadata.txt
+  --i-table /home/gas51591/boba/16s/ca_analysis/16s-dada2-table-ca.qza \
+  --o-visualization /home/gas51591/boba/16s/ca_analysis/16s-dada2-table-ca.qzv \
+  --m-sample-metadata-file /home/gas51591/boba/16s/metadata/16s_metadata.txt
 
 qiime feature-table tabulate-seqs \
---i-data /home/gas51591/boba/16s/analysis/16s-denoised-rep-sequences.qza \
---o-visualization /home/gas51591/boba/16s/analysis/16s-denoised-rep-sequences.qzv
+  --i-data /home/gas51591/boba/16s/ca_analysis/data-sequences-ca-trimmed-dada2.qza \
+  --o-visualization /home/gas51591/boba/16s/ca_analysis/data-sequences-ca-tabulated.qzv
 
 
 
@@ -91,15 +90,15 @@ OUTPUT=/home/gas51591/boba/16s/analysis
 
 # Script
 qiime tools import \
---type FeatureData[Sequence] \
---input-path /home/gas51591/boba/16s/tax/SILVA138-nr99_sequences_16S.fasta \
---output-path /home/gas51591/boba/16s/tax/SILVA138-nr99_sequences_16S.qza
+  --type FeatureData[Sequence] \
+  --input-path /home/gas51591/boba/16s/tax/SILVA138-nr99_sequences_16S.fasta \
+  --output-path /home/gas51591/boba/16s/tax/SILVA138-nr99_sequences_16S.qza
 
 qiime tools import \
---type FeatureData[Taxonomy] \
---input-format HeaderlessTSVTaxonomyFormat \
---input-path /home/gas51591/boba/16s/tax/SILVA138-nr99_taxonomy_16S.txt \
---output-path /home/gas51591/boba/16s/tax/SILVA138-nr99_ref_taxonomy_16S.qza
+  --type FeatureData[Taxonomy] \
+  --input-format HeaderlessTSVTaxonomyFormat \
+  --input-path /home/gas51591/boba/16s/tax/SILVA138-nr99_taxonomy_16S.txt \
+  --output-path /home/gas51591/boba/16s/tax/SILVA138-nr99_ref_taxonomy_16S.qza
 
 
 
@@ -111,27 +110,27 @@ OUTPUT=/home/gas51591/boba/16s/analysis
 
 # Script
 qiime feature-classifier classify-consensus-blast \
---i-query /home/gas51591/boba/16s/analysis/16s-denoised-rep-sequences.qza \
---i-reference-reads /home/gas51591/boba/16s/tax/SILVA138-nr99_sequences_16S.qza \
---i-reference-taxonomy /home/gas51591/boba/16s/tax/SILVA138-nr99_ref_taxonomy_16S.qza \
---p-maxaccepts 10 \
---p-perc-identity .80 \
---p-min-consensus .51 \
---o-classification /home/gas51591/boba/16s/analysis/16s-rep-sequences-taxonomy.qza \
---o-search-results /home/gas51591/boba/16s/analysis/16s-rep-sequences-tax-search-results.qza
+  --i-query /home/gas51591/boba/16s/ca_analysis/data-sequences-ca-trimmed-dada2.qza \
+  --i-reference-reads /home/gas51591/boba/16s/tax/SILVA138-nr99_sequences_16S.qza \
+  --i-reference-taxonomy /home/gas51591/boba/16s/tax/SILVA138-nr99_ref_taxonomy_16S.qza \
+  --p-maxaccepts 10 \
+  --p-min-consensus .51 \
+  --p-perc-identity .80 \
+  --o-classification /home/gas51591/boba/16s/ca_analysis/ca-16s-rep-sequences-taxonomy.qza \
+  --o-search-results /home/gas51591/boba/16s/ca_analysis/ca-16s-rep-sequences-tax-search-results.qza
 
 
 
 # Step 8 Taxonomic Visualization 
 # Activate QIIME2/2024.2-amplicon and add Path Variables 
 module load QIIME2/2024.2-amplicon
-INPUT=/home/gas51591/boba/16s/analysis/16s-rep-sequences-taxonomy.qza
-OUTPUT=/home/gas51591/boba/16s/analysis/16s-rep-sequences-taxonomy.qzv
+INPUT=/home/gas51591/boba/16s/analysis/ca-16s-rep-sequences-taxonomy.qza
+OUTPUT=/home/gas51591/boba/16s/analysis/ca-16s-rep-sequences-taxonomy.qzv
 
 # Script
 qiime metadata tabulate \
---m-input-file $INPUT \
---o-visualization $OUTPUT
+  --m-input-file $INPUT \
+  --o-visualization $OUTPUT
 
 
 
@@ -144,8 +143,8 @@ OUTPUT=/home/gas51591/boba/16s/analysis/
 
 # Script
 qiime phylogeny  align-to-tree-mafft-fasttree \
---i-sequences /home/gas51591/boba/16s/analysis/16s-denoised-rep-sequences.qza \
---o-alignment  /home/gas51591/boba/16s/analysis/aligned-16S-rep-seqs.qza \
---o-masked-alignment /home/gas51591/boba/16s/analysis/16s-masked-aligned-rep-seqs.qza \
---o-tree /home/gas51591/boba/16s/analysis/unrooted-16s-tree.qza \
---o-rooted-tree /home/gas51591/boba/16s/analysis/rooted-16s-tree.qza
+  --i-sequences /home/gas51591/boba/16s/ca_analysis/data-sequences-ca-trimmed-dada2.qza \
+  --o-alignment  /home/gas51591/boba/16s/ca_analysis/ca-aligned-16S-rep-seqs.qza \
+  --o-masked-alignment /home/gas51591/boba/16s/ca_analysis/ca-16s-masked-aligned-rep-seqs.qza \
+  --o-tree /home/gas51591/boba/16s/ca_analysis/ca-unrooted-16s-tree.qza \
+  --o-rooted-tree /home/gas51591/boba/16s/ca_analysis/ca-rooted-16s-tree.qza
